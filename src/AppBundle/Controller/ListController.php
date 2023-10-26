192807f9326cd16e8a3d694d4ce65c4e7c83d50e
AnTrAxVC74 <019000568@upsrj.edu.mx>
2023-10-25 20:13:52 -0600

    filters have been added

--- src/AppBundle/Controller/ListController.php
+++ src/AppBundle/Controller/ListController.php
@@ -9,6 +9,8 @@ use AppBundle\Entity\Installation;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityManagerInterface;
+
 class ListController extends Controller
 {
 
@@ -97,7 +99,7 @@ class ListController extends Controller
                     'class' => 'form-control'
                     )
                 )
-            )
+            ) 
             ->add('save', SubmitType::class, array(
                 'label' => 'Filter',
                 'attr' => array(
@@ -112,15 +114,62 @@ class ListController extends Controller
 
             $manufacturer = $this->getDoctrine()->getRepository('AppBundle:Manufacturer')->find($installation->getInstallationCompany());
             $manufacturerName = $manufacturer->getName();
+            $em = $this->getDoctrine()->getManager();
 
             $installations = $this->getDoctrine()
                 ->getRepository('AppBundle:Installation')
                 ->findInstManufacturer($manufacturerName);
+            
+                //Busca los Installed
+            $installed = $em->createQueryBuilder()
+                ->select('i')
+                ->from('AppBundle:Installation', 'i')
+                ->where('i.installationStatus = :estatus')
+                ->andWhere('i.installationCompany = :manufacturer')
+                ->setParameters(
+                    [
+                        'estatus' => 'Installed', 
+                        'manufacturer' => $manufacturerName
+                    ]
+                )
+                ->getQuery();
+            $result_installed = $installed->getResult();
+
+            //Busca los Pending
+            $property = 'installationStatus';
+            $wordPending = 'Pending';
+            $pending = $em->createQueryBuilder()
+                ->select('i')
+                ->from('AppBundle:Installation', 'i')
+                ->where('i.' . $property . ' LIKE :wordPending')
+                ->setParameter('wordPending','%'.$wordPending.'%')
+                ->andWhere('i.installationCompany = :manufacturer')
+                ->setParameter('manufacturer' , $manufacturerName)
+                ->getQuery();
+            $result_pending = $pending->getResult();
+
+            //Busca los Pending
+            $property = 'installationStatus';
+            $wordCancelled = 'Cancelled';
+            $cancelled = $em->createQueryBuilder()
+                ->select('i')
+                ->from('AppBundle:Installation', 'i')
+                ->where('i.' . $property . ' LIKE :wordCancelled')
+                ->setParameter('wordCancelled','%'.$wordCancelled.'%')
+                ->andWhere('i.installationCompany = :manufacturer')
+                ->setParameter('manufacturer' , $manufacturerName)
+                ->getQuery();
+            $result_cancelled = $cancelled->getResult();
+
+
 
             return $this->render('List/Tables/FilterByManufacturerList.html.twig', array(
                 'installation' => $installations,
                 'role' => $roleApp,
-                'user' => $user
+                'user' => $user,
+                'installed' => $result_installed,
+                'pending' => $result_pending,
+                'cancelled' => $result_cancelled
             ));
         }
 
